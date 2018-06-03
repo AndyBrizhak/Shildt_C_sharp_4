88a41eb11720ac6c702faa65446441679f9a40ee
AndyBrizhak <andrewbrizhak@gmail.com>
2018-06-03 10:00:58 +0300

    + isodd

---
+++ IsOdd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort num;
+            num = 10;
+
+            if ((num & 1) == 1)
+                Console.WriteLine("He выводится.");
+            num = 11;
+            if ((num & 1) == 1)
+                Console.WriteLine(num + " — нечетное число.");
+            Console.ReadKey();
+        }
+    }
+}
