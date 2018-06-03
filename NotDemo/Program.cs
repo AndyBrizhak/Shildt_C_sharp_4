c68174d4bfd7bc1669470db846eef13ff2d233cc
AndyBrizhak <andrewbrizhak@gmail.com>
2018-06-03 11:13:13 +0300

    +NotDemo

---
+++ NotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte b = -34;
+            for (int t = 128; t > 0; t = t / 2)
+            {
+                if ((b & t) != 0) Console.Write("1 ");
+                if ((b & t) == 0) Console.Write("0 ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+            // обратить все биты
+            b = (sbyte)~b;
+            for (int t = 128; t > 0; t = t / 2)
+            {
+                if ((b & t) != 0) Console.Write("1 ");
+                if ((b & t) == 0) Console.Write("0 ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
