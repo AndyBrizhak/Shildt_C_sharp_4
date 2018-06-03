8ba2953248560e0f10eae3032ae124ca1eaa70ba
AndyBrizhak <andrewbrizhak@gmail.com>
2018-06-03 10:09:53 +0300

    +ShowBits

---
+++ ShowBits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t;
+            byte val;
+            val = 123;
+            for (t = 128; t > 0; t = t / 2)
+            {
+                if ((val & t) != 0) Console.Write("1 ");
+                if ((val & t) == 0) Console.Write("0 ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
