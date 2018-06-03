7e4b526245ea8a74458ba0ac564b4cb5ad6fb64a
AndyBrizhak <andrewbrizhak@gmail.com>
2018-06-03 14:44:02 +0300

    +ternary

---
+++ ternary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result, i=0;
+            result = i != 0 ? 100 / i : 0;
+            //bool condition=true;
+            //condition   ? return condition true:    return condition false;
+        }
+    }
+}
